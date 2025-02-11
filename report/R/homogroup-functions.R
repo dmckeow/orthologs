a42e04dea6e2eb664af7ded038ea6748c3e79be3
dmckeow <deanandrewmckeown@gmail.com>
2025-02-11 16:22:29 +0100

    prepping rmd data

--- report/R/homogroup-functions.R
+++ report/R/homogroup-functions.R
@@ -15,99 +15,6 @@ clean_pfam_scan_file <- function(file_path, header) {
   return(data)
 }
 
-prepare_all_data <- function(
-  results_dir = NULL,
-  samplesheet_fpath = NULL,
-  orthogroups_path = NULL,
-  functional_annotations = NULL,
-  OG_source = NULL  # Add new parameter for OG_source
-) {
-
-  # Read the samplesheet
-  samplesheet <- read.delim(samplesheet_fpath, sep = ",")
-  
-  # Read the orthogroups data
-  orthogroups <- cogeqc::read_orthogroups(orthogroups_path)
-  
-  # Get all defline info 
-  file_paths <- list.files(paste0(results_dir, "prefilter/initial/defline_info/"), 
-                            pattern = "*.csv", full.names = TRUE)
-  defline_info <- lapply(file_paths, read.csv)
-  defline_info <- do.call(rbind, defline_info)
-
-  # Link functional annotations to sequence ids
-  merged_data <- defline_info %>%
-    left_join(functional_annotations, by = c("parent_seq" = "seq_id"))
-  
-  # Merge with samplesheet and rename columns
-  merged_data <- merged_data %>%
-    left_join(samplesheet, by = c("id" = "id")) %>%
-    rename(
-      supergroup = taxonomy
-    ) %>%
-    select(-fasta)
-
-  # Merge with orthogroups data
-  merged_data <- merged_data %>%
-    left_join(orthogroups, by = c("clean_seq" = "Gene")) %>%
-    select(-Species)
-  
-  # Add the new OG_source column to the merged data
-  merged_data <- merged_data %>%
-    mutate(OG_source = OG_source)  # Fill the column with the passed value
-  
-  # Return the merged data with the new OG_source column
-  return(merged_data)
-}
-
-
-prepare_cogeqc_inputs <- function(
-    input_df = NULL,
-    species_col = NULL,
-    gene_col = NULL,
-    annot_col = NULL,
-    annot_filter_col = NULL,
-    annot_filter_value = NULL
-  ) {
-    orthogroups <- input_df %>%
-      filter(!is.na(Orthogroup)) %>%
-      select(
-          Orthogroup,
-          {{species_col}},
-          {{gene_col}}
-        ) %>%
-      rename(
-        Species = {{species_col}},
-        Gene = {{gene_col}}
-      ) %>%
-      unique()
-
-      annotations <- input_df %>%
-    filter(!is.na({{annot_col}})) %>%  # Filter out rows with NA in annot_col
-    filter({{annot_filter_col}} == annot_filter_value) %>%  # Filter based on annot_filter_col and value
-    select(
-      {{species_col}},
-      {{gene_col}},
-      {{annot_col}}
-    ) %>%
-    rename(
-      Species = {{species_col}},
-      Gene = {{gene_col}},
-      Annotation = {{annot_col}}
-    ) %>%
-    unique()
-  # split into lists by species
-  annotations_split <- annotations %>%
-    split(annotations$Species) %>%
-    lapply(function(df) {
-      df %>%
-        select(-Species)  # Remove the 'Species' column from each split dataframe
-    })
-  
-  # Return both dataframes (orthogroups and annotations_list)
-  return(list(orthogroups = orthogroups, annotations = annotations_split))
-  }
-
 
 get_cogeqc_hscores <- function(og_data, annotation) {
   # Process each species
@@ -173,7 +80,8 @@ compare_homogeneity_scores <- function(..., comps = NULL) {
   # Visualize distributions with significant differences highlighted
   OUT_distros <- ggviolin(
     H_combined, y = "Score_scaled", x = "Source", 
-    orientation = "horiz", trim = TRUE, add = c("boxplot", "mean"), 
+    trim = TRUE, 
+    add = "jitter",
     fill = "Source", add.params = list(fill = "white"), palette = "jama"
   ) +
     theme(legend.position = "none") +
@@ -187,66 +95,6 @@ compare_homogeneity_scores <- function(..., comps = NULL) {
     og_hscores_scaled_all = H_combined,
     og_hscores_scaled_violin = OUT_distros
   ))
-}
-
-
-compare_wilcox <- function(data, form, ref = NULL) {
-    # Wilcoxon test - greater and less alternatives
-    wilcoxtest_greater <- tibble::as_tibble(data) %>%
-        rstatix::wilcox_test(
-            formula(form), p.adjust.method = "BH", ref.group = ref,
-            alternative = "greater"
-        )
-    pg <- ifelse("p.adj" %in% names(wilcoxtest_greater), "p.adj", "p")
-    wilcoxtest_greater <- wilcoxtest_greater %>% dplyr::select(
-            group1, group2, n1, n2, padj_greater = all_of(pg)
-        )
-    
-    wilcoxtest_less <- tibble::as_tibble(data) %>%
-        rstatix::wilcox_test(
-            formula(form), p.adjust.method = "BH", ref.group = ref,
-            alternative = "less"
-        )
-    pl <- ifelse("p.adj" %in% names(wilcoxtest_less), "p.adj", "p")
-    wilcoxtest_less <- wilcoxtest_less %>% dplyr::select(
-        group1, group2, n1, n2, padj_less = all_of(pl)
-    )
-    
-    wilcox_summary <- dplyr::inner_join(wilcoxtest_greater, wilcoxtest_less) %>%
-        dplyr::mutate(padj_interpretation = dplyr::case_when(
-            padj_less < 0.05 ~ "less",
-            padj_greater < 0.05 ~ "greater",
-            TRUE ~ "ns"
-        ))
-    
-    # Effect sizes for Wilcoxon test - greater and less alternatives
-    
-    effsize <- tibble::as_tibble(data) %>%
-        rstatix::wilcox_effsize(
-            formula(form), ref.group = ref,
-        ) %>%
-        dplyr::select(
-            group1, group2, effsize, magnitude
-        )
-    
-    
-    result <- as.data.frame(inner_join(wilcox_summary, effsize))
-        
-    return(result)
-}
-
-
-filter_comparison_wilcox <- function(compare_output) {
-    
-    filtered_df <- compare_output |>
-        dplyr::filter(padj_interpretation != "ns") |>
-        mutate(padj = case_when(
-            padj_interpretation == "greater" ~ padj_greater,
-            padj_interpretation == "less" ~ padj_less
-        )) |>
-        dplyr::select(group1, group2, n1, n2, padj, effsize, magnitude)
-    
-    return(filtered_df)
 }
 
 
@@ -322,3 +170,11 @@ create_presence_matrix <- function(df, group_col, value_col) {
 }
 
 
+get_mode <- function(x) {
+  uniq_x <- unique(x)
+  uniq_x[which.max(tabulate(match(x, uniq_x)))]
+}
+
+count_mode <- function(x, mode_value) {
+  sum(x == mode_value)
+}
